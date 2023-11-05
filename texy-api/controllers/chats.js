0142c1a6bb2ee6b104fa691494a582ffb603b80d
deluxesande <deluxesande@gmail.com>
2023-11-05 07:23:57 +0300

    CRUD functionality

--- texy-api/controllers/chats.js
+++ texy-api/controllers/chats.js
@@ -6,12 +6,54 @@ const getAllChats = asyncWrapper(async (req, res) => {
     res.status(200).json({ chats });
 });
 
+const getSingleChat = asyncWrapper(async (req, res) => {
+    const id = "6546b25accf64320e4ef967f";
+    const chat = await Chat.findOne({ _id: id });
+
+    if (!chat) {
+        res.status(404).json({ msg: `Chat with id ${id} does not exist.` });
+    }
+
+    res.status(200).json(chat);
+});
+
 const createChat = asyncWrapper(async (req, res) => {
-    const chat = await Chat.create({ sender: "Sean" });
+    const chat = await Chat.create({ sender: "John" });
     res.status(201).json(chat);
+});
+
+const updateChat = asyncWrapper(async (req, res) => {
+    const id = "6546b25accf64320e4ef967f";
+    const chat_to_send = { sender: "john" };
+
+    const chat = await Chat.findOneAndUpdate({ _id: id }, chat_to_send, {
+        new: true,
+        runValidators: true,
+    });
+
+    if (!chat) {
+        res.status(404).json({ msg: `Chat with id ${id} does not exist.` });
+    }
+
+    res.status(200).json(chat);
+});
+
+const deleteChat = asyncWrapper(async (req, res) => {
+    const id = "6546b25accf64320e4ef967f";
+
+    const chat = await Chat.findOneAndDelete({ _id: id });
+
+    if (!chat) {
+        res.status(404).json({ msg: `Chat with id ${id} does not exist.` });
+    }
+
+    res.status(200).json({ chat });
 });
 
 module.exports = {
     getAllChats,
     createChat,
+    getSingleChat,
+    updateChat,
+    deleteChat,
 };
--- texy-api/routes/chats.js
+++ texy-api/routes/chats.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAllChats, createChat } = require("../controllers/chats");
+const {
+    getAllChats,
+    createChat,
+    getSingleChat,
+    updateChat,
+    deleteChat,
+} = require("../controllers/chats");
 
 router.route("/").get(getAllChats).post(createChat);
+router.route("/:id").get(getSingleChat).patch(updateChat).delete(deleteChat);
 
 module.exports = router;
